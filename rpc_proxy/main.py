999b38430e01e13b253a038dde54b017227ded2b
root <shpal2000@gmail.com>
2020-09-20 22:26:14 +0000

    rpc-proxy

--- rpc_proxy/main.py
+++ rpc_proxy/main.py
@@ -2,49 +2,57 @@ from fastapi import FastAPI
 from typing import Optional, List
 from pydantic import BaseModel
 
-# from fastapi.responses import HTMLResponse
-
-
 import asyncio
 import uvicorn
+import json
+import sys
 
 
 app = FastAPI()
 
-class StopParam(BaseModel):
+class BaseParam(BaseModel):
+    rpc_ip: str
+    rpc_port: int
+
+class StopParam(BaseParam):
     timeout: int
 
-# @app.get('/', response_class=HTMLResponse)
-# async def root():
-#     reader, writer = await asyncio.open_connection('www.site.com'
-#                                                     , 80
-#                                                     , ssl=False)
-#     writer.write(f'GET / HTTP/1.1\r\nHost: www.site.com\r\n\r\n'.encode())
-#     await writer.drain()
+class EvSockStatsParam(BaseParam):
+    stats_group: str
 
-#     response = await reader.read (10000)
-
-#     writer.close()
-#     await writer.wait_closed()
-
-#     return HTMLResponse(content=response, status_code=200)
-
-
-@app.post('/stop')
-async def stop(params : StopParam):
-    return params
+@app.post('/start')
+async def start():
+    return {}
 
 @app.post('/abort')
 async def abort():
     return {}
 
+@app.post('/stop')
+async def stop(params : StopParam):
+    return params
+
 @app.get('/ev_sockstats')
-async def stats():
-    return {}
+async def ev_sockstats(params : EvSockStatsParam):
+    reader, writer = await asyncio.open_connection (params.rpc_ip
+                                                    , params.rpc_port
+                                                    , ssl=False)
+
+    writer.write('{"cmd" : "get_ev_sockstats"}'.encode())
+    await writer.drain()
+
+    response = await reader.read (-1)
+
+    writer.close()
+    await writer.wait_closed()
+
+    return json.loads (response)
 
 if __name__ == '__main__':
+    rpc_proxy_ip = sys.argv[1]
+    rpc_proxy_port = int(sys.argv[2])
     asyncio.run (uvicorn.run(app
-                                , host='0.0.0.0'
-                                , port=8000
+                                , host=rpc_proxy_ip
+                                , port=rpc_proxy_port
                                 , loop='asyncio'
                                 , debug=False))
