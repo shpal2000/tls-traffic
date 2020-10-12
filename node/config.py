de93ad53641ce2660c0e70daa4d16c3edc6491f8
root <shpal2000@gmail.com>
2020-10-12 04:48:45 +0000

    refactor

---
+++ node/config.py
@@ -0,0 +1,16 @@
+DB_CSTRING = '<>:27017'
+
+REGISTRY_DB_NAME = 'tgen_resgistry'
+
+RESULT_DB_NAME = 'tgen_result'
+
+STATS_TABLE = 'tgen_stats'
+
+CSTATE_TABLE = 'tgen_cstates'
+
+LIVE_STATS_TABLE = 'tgen_live_stats'
+
+RPC_IP_VETH1 = '192.168.1.1'
+RPC_IP_VETH2 = '192.168.1.2'
+RPC_PORT = 8081
+
